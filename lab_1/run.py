f8b6932e88da946a734359c87686b85e63321c45
PheeZz <jagubkoff@gmail.com>
2022-10-05 13:00:40 +0200

    done 2.3

--- lab_1/run.py
+++ lab_1/run.py
@@ -1,5 +1,8 @@
+from string import whitespace
+
+
 class PrettyPrinter():
-    def __init__(self, number):
+    def __init__(self, number: int):
         self._number = number
 
     # 2.1
@@ -14,15 +17,34 @@ class PrettyPrinter():
             print(*[str(i) for i in range(1, maximum+1)], sep='')
         print('\n\n')
 
-    # 2.2
+        # 2.2
     def triangle(self):
         print('#2.2:')
         triangle_list = list()
-
         for maximum in range(1, self._number+1):
             output_str = str()
             output_str += ''.join([str(i) for i in range(1, maximum+1)])
             output_str += ''.join([str(i) for i in range(maximum-1, 0, -1)])
+            triangle_list.append(output_str)
+
+        for item in triangle_list:
+            item = item.center(len(triangle_list[-1]), ' ')
+            print(item)
+
+    def triangle_round(self):
+        print('\n\n#2.3:')
+        triangle_list = list()
+        whitespaces = len(str(self._number))
+        for maximum in range(1, self._number+1):
+            output_str = str()
+            temp_list = [str(i) for i in range(1, maximum+1)]
+            temp_list_reversed = [str(i) for i in range(maximum-1, 0, -1)]
+
+            temp_list = [i.rjust(whitespaces, ' ') for i in temp_list]
+            temp_list_reversed = [i.rjust(whitespaces, ' ')
+                                  for i in temp_list_reversed]
+
+            output_str += ''.join(temp_list+temp_list_reversed)
             triangle_list.append(output_str)
 
         for item in triangle_list:
@@ -40,5 +62,5 @@ if __name__ == '__main__':
     _.int_output()
     _.str_output()
     _.triangle()
+    _.triangle_round()
     print('\nby Daniil Y. 2022')
-
